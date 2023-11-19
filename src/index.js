738d54696e9f5b5c1fa06972274683813be39e5a
Kreoniz <kreoniz@mail.ru>
2023-11-19 17:23:22 +0600

    Create basic drop-down menus

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import './reset.css';
 import './style.css';
+
+const dropDowns = document.querySelectorAll('.drop-down');
+console.log(dropDowns);
+
+dropDowns.forEach(dropDown => {
+  dropDown.querySelector('.drop-down-title').addEventListener('click', (e) => {
+    dropDown.classList.toggle('hidden');
+  });
+});
