cc9a516cd96324606d37a6d35ab8c5246499e29e
Kreoniz <kreoniz@mail.ru>
2023-12-06 12:00:28 +0600

    Add a semi-functional collapsing menu

--- src/index.js
+++ src/index.js
@@ -23,3 +23,85 @@ dropDowns.forEach((dropDown) => {
     dropDown.classList.toggle('closed');
   });
 });
+
+function getWidth() {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
+}
+
+function getHeight() {
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.documentElement.clientHeight
+  );
+}
+
+class CollapsingMenu {
+  constructor(shownId, hiddenId) {
+    this.shownMenu = document.querySelector(`#${shownId}`);
+    this.hiddenMenu = document.querySelector(`#${hiddenId}`);
+    this.items = [...this.hiddenMenu.childNodes];
+  }
+
+  get shownItems() {
+    const shownMenuItems = [];
+
+    // const screenWidth = window.screen.availWidth;
+    const screenWidth = getWidth();
+    let width = 0;
+
+    for (let i = 0; i < this.items.length; i += 1) {
+      width += this.items[i].offsetWidth;
+      if (width <= screenWidth) {
+        shownMenuItems.push(this.items[i]);
+      }
+    }
+
+    return shownMenuItems;
+  }
+
+  get hiddenItems() {
+    const hiddenMenuItems = [];
+
+    // const screenWidth = window.screen.availWidth;
+    const screenWidth = getWidth();
+    let width = 0;
+
+    for (let i = 0; i < this.items.length; i += 1) {
+      width += this.items[i].offsetWidth;
+      if (width > screenWidth) {
+        hiddenMenuItems.push(this.items[i]);
+      }
+    }
+
+    return hiddenMenuItems;
+  }
+
+  render() {
+    this.shownMenu.textContent = '';
+    this.hiddenMenu.textContent = '';
+    console.log(this.items);
+    console.log('Shown:', this.shownItems.map((e) => e.textContent), this.shownMenu.childNodes);
+    console.log('Hidden:', this.hiddenItems.map((e) => e.textContent), this.hiddenMenu.childNodes);
+
+    for (let i = 0; i < this.shownItems.length; i += 1) {
+      this.shownMenu.appendChild(this.shownItems[i]);
+    }
+    for (let i = 0; i < this.hiddenItems.length; i += 1) {
+      this.hiddenMenu.appendChild(this.hiddenItems[i]);
+    }
+  }
+}
+
+const menu = new CollapsingMenu('shown-menu', 'hidden-menu');
+menu.render();
+
+window.addEventListener('resize', () => menu.render());
